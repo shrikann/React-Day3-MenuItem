5c5dca895f5a85b1827790e59f3292878f3e4881
SHRINES K <128037088+shrikann@users.noreply.github.com>
2023-03-27 12:10:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Rowing from './Rowing';
+import React from 'react';
+function App() {
+  return (
+    <div
+      ClassName="Check in">
+      <Rowing/>
+    </div>
+  )
+}
+
+export default App;
